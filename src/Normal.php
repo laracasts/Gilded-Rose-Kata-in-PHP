4cb274562b015bce20a5d3332c95b92baafc3766
Jeffrey Way <jeffrey@jeffrey-way.com>
2015-10-26 17:16:19 -0400

    Fix situation when quality ends up negative

--- src/Normal.php
+++ src/Normal.php
@@ -7,15 +7,14 @@ class Normal extends Item
     public function tick()
     {
         $this->sellIn -= 1;
-
-        if ($this->quality == 0) {
-            return;
-        }
-
         $this->quality -= 1;
 
         if ($this->sellIn <= 0) {
             $this->quality -= 1;
+        }
+
+        if ($this->quality < 0) {
+            $this->quality = 0;
         }
     }
 }
