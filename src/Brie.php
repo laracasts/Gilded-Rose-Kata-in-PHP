4383eb12b1809319134c933fa3e054938fa3bf22
Jeffrey Way <jeffrey@jeffrey-way.com>
2015-10-26 17:38:44 -0400

    Refactor Brie code

--- src/Brie.php
+++ src/Brie.php
@@ -7,15 +7,14 @@ class Brie extends Item
     public function tick()
     {
         $this->sellIn -= 1;
-
-        if ($this->quality >= 50) {
-            return;
-        }
-
         $this->quality += 1;
 
-        if ($this->sellIn <= 0 && $this->quality < 50) {
+        if ($this->sellIn <= 0) {
             $this->quality += 1;
+        }
+
+        if ($this->quality > 50) {
+            $this->quality = 50;
         }
     }
 }
