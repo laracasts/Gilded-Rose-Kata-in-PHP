a6b41aa0591d3bfcae893e507b4edff68c32d96a
Jeffrey Way <jeffrey@jeffrey-way.com>
2015-10-26 14:11:35 -0400

    Add commented specs for Conjured

--- spec/GildedRoseSpec.php
+++ spec/GildedRoseSpec.php
@@ -3,47 +3,47 @@
 use App\Item;
 use App\GildedRose;
 
-// Quality = How valuable the item is
-// sellIn = The number of days you have to sell it, before it goes bad.
-// At the end of the day, the system updates the quality.
+/*
+ * Your work begins on LINE 249.
+ */
 
 describe('Gilded Rose', function () {
 
-    describe('#updateQuality', function () {
+    describe('#update', function () {
 
         context ('normal Items', function () {
 
             it ('updates normal items before sell date', function () {
-                $item = new GildedRose('normal', 10, 5); // quality, sell in X days
+                $item = GildedRose::of('normal', 10, 5); // quality, sell in X days
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(9);
                 expect($item->sellIn)->toBe(4);
             });
 
             it ('updates normal items on the sell date', function () {
-                $item = new GildedRose('normal', 10, 0);
+                $item = GildedRose::of('normal', 10, 0);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(8);
                 expect($item->sellIn)->toBe(-1);
             });
 
             it ('updates normal items after the sell date', function () {
-                $item = new GildedRose('normal', 10, -5);
+                $item = GildedRose::of('normal', 10, -5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(8);
                 expect($item->sellIn)->toBe(-6);
             });
 
             it ('updates normal items with a quality of 0', function () {
-                $item = new GildedRose('normal', 0, 5);
+                $item = GildedRose::of('normal', 0, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(0);
                 expect($item->sellIn)->toBe(4);
@@ -55,63 +55,63 @@ describe('Gilded Rose', function () {
         context('Brie Items', function () {
 
             it ('updates Brie items before the sell date', function () {
-                $item = new GildedRose('Aged Brie', 10, 5);
+                $item = GildedRose::of('Aged Brie', 10, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(11);
                 expect($item->sellIn)->toBe(4);
             });
 
             it ('updates Brie items before the sell date with maximum quality', function () {
-                $item = new GildedRose('Aged Brie', 50, 5);
+                $item = GildedRose::of('Aged Brie', 50, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(4);
             });
 
             it ('updates Brie items on the sell date', function () {
-                $item = new GildedRose('Aged Brie', 10, 0);
+                $item = GildedRose::of('Aged Brie', 10, 0);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(12);
                 expect($item->sellIn)->toBe(-1);
             });
 
             it ('updates Brie items on the sell date, near maximum quality', function () {
-                $item = new GildedRose('Aged Brie', 49, 0);
+                $item = GildedRose::of('Aged Brie', 49, 0);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(-1);
             });
 
             it ('updates Brie items on the sell date with maximum quality', function () {
-                $item = new GildedRose('Aged Brie', 50, 0);
+                $item = GildedRose::of('Aged Brie', 50, 0);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(-1);
             });
 
             it ('updates Brie items after the sell date', function () {
-                $item = new GildedRose('Aged Brie', 10, -10);
+                $item = GildedRose::of('Aged Brie', 10, -10);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(12);
                 expect($item->sellIn)->toBe(-11);
             });
 
              it ('updates Briem items after the sell date with maximum quality', function () {
-                $item = new GildedRose('Aged Brie', 50, -10);
+                $item = GildedRose::of('Aged Brie', 50, -10);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(-11);
@@ -123,27 +123,27 @@ describe('Gilded Rose', function () {
         context('Sulfuras Items', function () {
 
             it ('updates Sulfuras items before the sell date', function () {
-                $item = new GildedRose('Sulfuras, Hand of Ragnaros', 10, 5);
+                $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 10, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(10);
                 expect($item->sellIn)->toBe(5);
             });
 
             it ('updates Sulfuras items on the sell date', function () {
-                $item = new GildedRose('Sulfuras, Hand of Ragnaros', 10, 5);
+                $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 10, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(10);
                 expect($item->sellIn)->toBe(5);
             });
 
             it ('updates Sulfuras items after the sell date', function () {
-                $item = new GildedRose('Sulfuras, Hand of Ragnaros', 10, -1);
+                $item = GildedRose::of('Sulfuras, Hand of Ragnaros', 10, -1);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(10);
                 expect($item->sellIn)->toBe(-1);
@@ -153,55 +153,61 @@ describe('Gilded Rose', function () {
 
 
         context('Backstage Passes', function () {
+            /*
+                "Backstage passes", like aged brie, increases in Quality as it's SellIn
+                value approaches; Quality increases by 2 when there are 10 days or
+                less and by 3 when there are 5 days or less but Quality drops to
+                0 after the concert
+             */
             it ('updates Backstage pass items long before the sell date', function () {
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 10, 11);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 11);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(11);
                 expect($item->sellIn)->toBe(10);
             });
 
             it ('updates Backstage pass items close to the sell date', function () {
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 10, 10);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 10);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(12);
                 expect($item->sellIn)->toBe(9);
             });
 
             it ('updates Backstage pass items close to the sell data, at max quality', function () {
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 50, 10);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 50, 10);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(9);
             });
 
             it ('updates Backstage pass items very close to the sell date', function () {
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 10, 5);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(13); // goes up by 3
                 expect($item->sellIn)->toBe(4);
             });
 
             it ('updates Backstage pass items very close to the sell date, at max quality', function () {
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 50, 5);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 50, 5);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(4);
             });
 
             it ('updates Backstage pass items with one day left to sell', function () {
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 10, 1);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 1);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(13);
                 expect($item->sellIn)->toBe(0);
@@ -209,9 +215,9 @@ describe('Gilded Rose', function () {
 
             it ('updates Backstage pass items with one day left to sell, at max quality', function () {
 
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 50, 1);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 50, 1);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(50);
                 expect($item->sellIn)->toBe(0);
@@ -219,9 +225,9 @@ describe('Gilded Rose', function () {
 
             it ('updates Backstage pass items on the sell date', function () {
 
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 10, 0);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, 0);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(0);
                 expect($item->sellIn)->toBe(-1);
@@ -229,15 +235,74 @@ describe('Gilded Rose', function () {
 
             it ('updates Backstage pass items after the sell date', function () {
 
-                $item = new GildedRose('Backstage passes to a TAFKAL80ETC concert', 10, -1);
+                $item = GildedRose::of('Backstage passes to a TAFKAL80ETC concert', 10, -1);
 
-                $item->updateQuality();
+                $item->update();
 
                 expect($item->quality)->toBe(0);
                 expect($item->sellIn)->toBe(-2);
             });
 
         });
+
+
+        // context ("Conjured Items", function () {
+
+        //     it ('updates Conjured items before the sell date', function () {
+        //         $item = GildedRose::of('Conjured Mana Cake', 10, 10);
+
+        //         $item->update();
+
+        //         expect($item->quality)->toBe(8);
+        //         expect($item->sellIn)->toBe(9);
+        //     });
+
+        //     it ('updates Conjured items at zero quality', function () {
+        //         $item = GildedRose::of('Conjured Mana Cake', 0, 10);
+
+        //         $item->update();
+
+        //         expect($item->quality)->toBe(0);
+        //         expect($item->sellIn)->toBe(9);
+        //     });
+
+        //     it ('updates Conjured items on the sell date', function () {
+        //         $item = GildedRose::of('Conjured Mana Cake', 10, 0);
+
+        //         $item->update();
+
+        //         expect($item->quality)->toBe(6);
+        //         expect($item->sellIn)->toBe(-1);
+        //     });
+
+        //     it ('updates Conjured items on the sell date at 0 quality', function () {
+        //         $item = GildedRose::of('Conjured Mana Cake', 0, 0);
+
+        //         $item->update();
+
+        //         expect($item->quality)->toBe(0);
+        //         expect($item->sellIn)->toBe(-1);
+        //     });
+
+        //     it ('updates Conjured items after the sell date', function () {
+        //         $item = GildedRose::of('Conjured Mana Cake', 10, -10);
+
+        //         $item->update();
+
+        //         expect($item->quality)->toBe(6);
+        //         expect($item->sellIn)->toBe(-11);
+        //     });
+
+        //     it ('updates Conjured items after the sell date at zero quality', function () {
+        //         $item = GildedRose::of('Conjured Mana Cake', 0, -10);
+
+        //         $item->update();
+
+        //         expect($item->quality)->toBe(0);
+        //         expect($item->sellIn)->toBe(-11);
+        //     });
+
+        // });
 
     });
 
