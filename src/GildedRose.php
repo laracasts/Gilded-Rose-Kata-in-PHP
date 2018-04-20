5cef0ef72e5ef5bbec8f4dff27c2ef3c40869aba
Adriano D'Alo <Adriano.Dalo@theiconic.com.au>
2018-04-20 12:21:52 +1000

    [adriano] GildedRose Kata - Moved logic at the top

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -17,38 +17,45 @@ class GildedRose
         $this->sellIn = $sellIn;
     }
 
-    public static function of($name, $quality, $sellIn) {
+    public static function of($name, $quality, $sellIn)
+    {
         return new static($name, $quality, $sellIn);
     }
 
     public function tick()
     {
-        if ($this->name != 'Aged Brie' AND $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            $this->quality = ($this->name != 'Sulfuras, Hand of Ragnaros' AND $this->quality > 0) ? $this->quality - 1 : $this->quality;
+        if ($this->name == 'Conjuras') {
+            $this->quality = $this->sellIn < 0 ? $this->quality - 4 : $this->quality - 2;
+            $this->sellIn = $this->sellIn -1;
         } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
 
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    $this->quality = ($this->sellIn < 11 AND $this->quality < 50) ? $this->quality + 1 : $this->quality;
-                    $this->quality = ($this->sellIn < 6 AND $this->quality < 50) ? $this->quality + 1 : $this->quality;
-                }
-            }
-        }
-
-        $this->sellIn = ($this->name != 'Sulfuras, Hand of Ragnaros') ? $this->sellIn - 1 : $this->sellIn;
-
-
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    $this->quality = ($this->quality > 0 AND $this->name != 'Sulfuras, Hand of Ragnaros') ? $this->quality - 1 : $this->quality - 1;
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
+            if ($this->name != 'Aged Brie' AND $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                $this->quality = ($this->name != 'Sulfuras, Hand of Ragnaros' AND $this->quality > 0) ? $this->quality - 1 : $this->quality;
             } else {
                 if ($this->quality < 50) {
                     $this->quality = $this->quality + 1;
+
+                    if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        $this->quality = ($this->sellIn < 11 AND $this->quality < 50) ? $this->quality + 1 : $this->quality;
+                        $this->quality = ($this->sellIn < 6 AND $this->quality < 50) ? $this->quality + 1 : $this->quality;
+                    }
+                }
+            }
+
+            $this->sellIn = ($this->name != 'Sulfuras, Hand of Ragnaros') ? $this->sellIn - 1 : $this->sellIn;
+
+
+            if ($this->sellIn < 0) {
+                if ($this->name != 'Aged Brie') {
+                    if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
+                        $this->quality = ($this->quality > 0 AND $this->name != 'Sulfuras, Hand of Ragnaros') ? $this->quality - 1 : $this->quality;
+                    } else {
+                        $this->quality = $this->quality - $this->quality;
+                    }
+                } else {
+                    if ($this->quality < 50) {
+                        $this->quality = $this->quality + 1;
+                    }
                 }
             }
         }
