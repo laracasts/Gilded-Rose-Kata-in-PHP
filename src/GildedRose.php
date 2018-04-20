b09dddcbfdc58784d6229a339a2a08d0d4fbcea6
Adriano D'Alo <Adriano.Dalo@theiconic.com.au>
2018-04-20 10:59:13 +1000

    [adriano] GildedRose Kata - Refactored  if statement with ternary operator

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -23,12 +23,8 @@ class GildedRose
 
     public function tick()
     {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
         if ($this->name != 'Aged Brie' AND $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
+            $this->quality = ($this->name != 'Sulfuras, Hand of Ragnaros' AND $this->quality > 0) ? $this->quality - 1 : $this->quality;
         } else {
             if ($this->quality < 50) {
                 $this->quality = $this->quality + 1;
