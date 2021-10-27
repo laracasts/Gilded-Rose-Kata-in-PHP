f60fd6d38ad1c18d33c574f83de704794ebee0ad
bniess <bniess@sevanova.com>
2021-10-27 09:05:09 +0200

    Started to split the main class into multiple calculator classes and initialized the aged brie class

---
+++ src/Calculators/AgedBrieCalculator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Calculators;
+
+
+class AgedBrieCalculator extends DefaultCalculator
+{
+
+    public function tick(): void
+    {
+        if ($this->quality < 50) {
+            $this->quality = $this->quality + 1;
+        }
+
+        $this->sellIn = $this->sellIn - 1;
+
+        if ($this->sellIn < 0) {
+            if ($this->quality < 50) {
+                $this->quality = $this->quality + 1;
+            }
+        }
+    }
+}
--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,70 +4,15 @@ namespace App;
 
 class GildedRose
 {
-    public $name;
-
-    public $quality;
-
-    public $sellIn;
-
-    public function __construct($name, $quality, $sellIn)
+    public static function of($name, $quality, $sellIn)
     {
-        $this->name = $name;
-        $this->quality = $quality;
-        $this->sellIn = $sellIn;
-    }
-
-    public static function of($name, $quality, $sellIn) {
-        return new static($name, $quality, $sellIn);
-    }
-
-    public function tick()
-    {
-        if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($this->quality > 0) {
-                if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                    $this->quality = $this->quality - 1;
-                }
-            }
-        } else {
-            if ($this->quality < 50) {
-                $this->quality = $this->quality + 1;
-
-                if ($this->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->sellIn < 11) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                    if ($this->sellIn < 6) {
-                        if ($this->quality < 50) {
-                            $this->quality = $this->quality + 1;
-                        }
-                    }
-                }
-            }
+        // TODO: Fix OCP issue => Replace with dynamic values
+        switch ($name) {
+            case 'Aged Brie':
+                return new \App\Calculators\AgedBrieCalculator($name, $quality, $sellIn);
+            default:
+                return new \App\Calculators\DefaultCalculator($name, $quality, $sellIn);
         }
 
-        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-            $this->sellIn = $this->sellIn - 1;
-        }
-
-        if ($this->sellIn < 0) {
-            if ($this->name != 'Aged Brie') {
-                if ($this->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($this->quality > 0) {
-                        if ($this->name != 'Sulfuras, Hand of Ragnaros') {
-                            $this->quality = $this->quality - 1;
-                        }
-                    }
-                } else {
-                    $this->quality = $this->quality - $this->quality;
-                }
-            } else {
-                if ($this->quality < 50) {
-                    $this->quality = $this->quality + 1;
-                }
-            }
-        }
     }
 }
