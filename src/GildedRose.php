8343e135823622d3dd204805bf5d1b9fcea8a79a
bniess <bniess@sevanova.com>
2021-10-27 10:03:30 +0200

    Removed switch and moved products data to a json file to fix OCP issue

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -8,17 +8,16 @@ class GildedRose
 {
     public static function of($name, $quality, $sellIn)
     {
-        // TODO: Fix OCP issue => Replace with dynamic values
-        switch ($name) {
-            case 'Aged Brie':
-                return new \App\Calculators\AgedBrieCalculator($name, $quality, $sellIn);
-            case 'Sulfuras, Hand of Ragnaros':
-                return new \App\Calculators\SulfurasCalculator($name, $quality, $sellIn);
-            case 'Backstage passes to a TAFKAL80ETC concert':
-                return new \App\Calculators\BackstageCalculator($name, $quality, $sellIn);
-            default:
-                return new \App\Calculators\DefaultCalculator($name, $quality, $sellIn);
+        $config = json_decode(file_get_contents('./config/products.json'), true);
+        foreach ($config['products'] as $productConfig) {
+            if ($name !== $productConfig['name']) {
+                continue;
+            }
+
+            return new $productConfig['calculator']($name, $quality, $sellIn);
         }
+
+        return new \App\Calculators\DefaultCalculator($name, $quality, $sellIn);
 
     }
 }
